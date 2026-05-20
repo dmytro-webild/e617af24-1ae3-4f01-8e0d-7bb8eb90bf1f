70486b935b28df8eb3487b0daae191ccffdbaf89
bender <kudin.dima@gmail.com>
2026-05-20 15:06:40 +0000

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,9 +47,9 @@ export default function LandingPage() {
       title="Authentic Japanese Vibes in Davie"
       description="Experience our signature Mochi Donuts, savory Yakitori, and crispy Corn Dogs. Made fresh daily with a modern twist."
       mediaItems={[
-        { imageSrc: "http://img.b2bpic.net/free-photo/flat-lay-composition-tasty-donuts_23-2148194234.jpg", imageAlt: "Fresh mochi donuts display" },
-        { imageSrc: "http://img.b2bpic.net/free-photo/fresh-yummy-white-chocolate-donuts-red-background_23-2147895888.jpg", imageAlt: "Signature mochi donut" },
-        { imageSrc: "http://img.b2bpic.net/free-photo/fried-chicken-meat-seafood-barbecue-with-vegetables-wooden-skewers-plate-sauce-top-view_1253-954.jpg", imageAlt: "Traditional yakitori skewers" },
+        { imageSrc: "http://img.b2bpic.net/free-photo/flat-lay-composition-tasty-donuts_23-2148194234.jpg?_wi=1", imageAlt: "Fresh mochi donuts display" },
+        { imageSrc: "http://img.b2bpic.net/free-photo/fresh-yummy-white-chocolate-donuts-red-background_23-2147895888.jpg?_wi=1", imageAlt: "Signature mochi donut" },
+        { imageSrc: "http://img.b2bpic.net/free-photo/fried-chicken-meat-seafood-barbecue-with-vegetables-wooden-skewers-plate-sauce-top-view_1253-954.jpg?_wi=1", imageAlt: "Traditional yakitori skewers" },
         { imageSrc: "http://img.b2bpic.net/free-photo/top-view-delicious-corn-dog_23-2149387967.jpg", imageAlt: "Crispy cheese corn dog" },
         { imageSrc: "http://img.b2bpic.net/free-photo/top-view-glazed-donuts_23-2148628295.jpg", imageAlt: "Creme brulee donut" },
         { imageSrc: "http://img.b2bpic.net/free-photo/set-sweet-doughnuts-with-sprinkles_114579-53452.jpg", imageAlt: "Matcha mochi donut" }
@@ -68,7 +68,7 @@ export default function LandingPage() {
         { title: "Fusion Favorites", description: "Crispy corn dogs and savory yakitori." },
         { title: "LGBTQ+ Friendly", description: "A welcoming space for everyone." }
       ]}
-      imageSrc="http://img.b2bpic.net/free-photo/girl-wearing-floral-dress-wearing-hat-sitting_1150-19274.jpg"
+      imageSrc="http://img.b2bpic.net/free-photo/girl-wearing-floral-dress-wearing-hat-sitting_1150-19274.jpg?_wi=1"
       mediaAnimation="slide-up"
     />
   </div>
@@ -80,7 +80,7 @@ export default function LandingPage() {
       gridVariant="uniform-all-items-equal"
       useInvertedBackground={false}
       features={[
-        { title: "Signature Mochi Donuts", description: "Chewy, airy, and full of flavor.", imageSrc: "http://img.b2bpic.net/free-photo/front-view-different-ingredients-cips-flakes-nuts-grey-background-meal-snack-breakfast-color_140725-110132.jpg" },
+        { title: "Signature Mochi Donuts", description: "Chewy, airy, and full of flavor.", imageSrc: "http://img.b2bpic.net/free-photo/front-view-different-ingredients-cips-flakes-nuts-grey-background-meal-snack-breakfast-color_140725-110132.jpg?_wi=1" },
         { title: "Crispy Street Food", description: "The best Korean-style cheese corn dogs.", imageSrc: "http://img.b2bpic.net/free-photo/top-view-hand-holding-corn-dog_23-2149929412.jpg" },
         { title: "Cozy Atmosphere", description: "Decorated like a Japanese city street.", imageSrc: "http://img.b2bpic.net/free-photo/man-ignoring-woman-while-talking-phone_107420-65720.jpg" }
       ]}
@@ -128,11 +128,11 @@ export default function LandingPage() {
       textboxLayout="default"
       useInvertedBackground={false}
       testimonials={[
-        { id: "t1", name: "Nae McQueen", date: "6 months ago", title: "Local Guide", quote: "Decorated like a Japanese city! Unique and cool.", tag: "Excellent", avatarSrc: "http://img.b2bpic.net/free-photo/medium-shot-people-sitting-together_23-2149366418.jpg", imageSrc: "http://img.b2bpic.net/free-photo/flat-lay-composition-tasty-donuts_23-2148194234.jpg", imageAlt: "happy customer dining cafe" },
-        { id: "t2", name: "Sarah Jenkins", date: "1 week ago", title: "Donut Lover", quote: "Matcha and black sesame were my favorites.", tag: "Tasty", avatarSrc: "http://img.b2bpic.net/free-photo/close-up-shot-bearded-businessman-light-pink-formal-shirt-covers-eyes-with-two-delicious-doughnuts-clenches-teeth-feels-temptation_273609-24083.jpg", imageSrc: "http://img.b2bpic.net/free-photo/girl-wearing-floral-dress-wearing-hat-sitting_1150-19274.jpg", imageAlt: "happy customer dining cafe" },
-        { id: "t3", name: "David Kim", date: "2 weeks ago", title: "Regular", quote: "Fantastic food and service with a smile.", tag: "Friendly", avatarSrc: "http://img.b2bpic.net/free-photo/happy-cooking_1098-13267.jpg", imageSrc: "http://img.b2bpic.net/free-photo/front-view-different-ingredients-cips-flakes-nuts-grey-background-meal-snack-breakfast-color_140725-110132.jpg", imageAlt: "happy customer dining cafe" },
-        { id: "t4", name: "Maria Lopez", date: "1 month ago", title: "Foodie", quote: "The cheese corn dogs are absolutely addictive.", tag: "Great", avatarSrc: "http://img.b2bpic.net/free-photo/happy-birthday-me-portrait-upbeat-excited-hispanic-man-with-dreads-celebrating-bday-holding-pl_1258-109346.jpg", imageSrc: "http://img.b2bpic.net/free-photo/fresh-yummy-white-chocolate-donuts-red-background_23-2147895888.jpg", imageAlt: "happy customer dining cafe" },
-        { id: "t5", name: "James Chen", date: "3 months ago", title: "Daily Visitor", quote: "The best mochi donuts in all of Florida.", tag: "Unique", avatarSrc: "http://img.b2bpic.net/free-photo/smiling-asian-girl-barista-giving-order-client-holding-takeaway-coffee-cup-wearing-apron-working_1258-199319.jpg", imageSrc: "http://img.b2bpic.net/free-photo/fried-chicken-meat-seafood-barbecue-with-vegetables-wooden-skewers-plate-sauce-top-view_1253-954.jpg", imageAlt: "happy customer dining cafe" }
+        { id: "t1", name: "Nae McQueen", date: "6 months ago", title: "Local Guide", quote: "Decorated like a Japanese city! Unique and cool.", tag: "Excellent", avatarSrc: "http://img.b2bpic.net/free-photo/medium-shot-people-sitting-together_23-2149366418.jpg", imageSrc: "http://img.b2bpic.net/free-photo/flat-lay-composition-tasty-donuts_23-2148194234.jpg?_wi=2", imageAlt: "happy customer dining cafe" },
+        { id: "t2", name: "Sarah Jenkins", date: "1 week ago", title: "Donut Lover", quote: "Matcha and black sesame were my favorites.", tag: "Tasty", avatarSrc: "http://img.b2bpic.net/free-photo/close-up-shot-bearded-businessman-light-pink-formal-shirt-covers-eyes-with-two-delicious-doughnuts-clenches-teeth-feels-temptation_273609-24083.jpg", imageSrc: "http://img.b2bpic.net/free-photo/girl-wearing-floral-dress-wearing-hat-sitting_1150-19274.jpg?_wi=2", imageAlt: "happy customer dining cafe" },
+        { id: "t3", name: "David Kim", date: "2 weeks ago", title: "Regular", quote: "Fantastic food and service with a smile.", tag: "Friendly", avatarSrc: "http://img.b2bpic.net/free-photo/happy-cooking_1098-13267.jpg", imageSrc: "http://img.b2bpic.net/free-photo/front-view-different-ingredients-cips-flakes-nuts-grey-background-meal-snack-breakfast-color_140725-110132.jpg?_wi=2", imageAlt: "happy customer dining cafe" },
+        { id: "t4", name: "Maria Lopez", date: "1 month ago", title: "Foodie", quote: "The cheese corn dogs are absolutely addictive.", tag: "Great", avatarSrc: "http://img.b2bpic.net/free-photo/happy-birthday-me-portrait-upbeat-excited-hispanic-man-with-dreads-celebrating-bday-holding-pl_1258-109346.jpg", imageSrc: "http://img.b2bpic.net/free-photo/fresh-yummy-white-chocolate-donuts-red-background_23-2147895888.jpg?_wi=2", imageAlt: "happy customer dining cafe" },
+        { id: "t5", name: "James Chen", date: "3 months ago", title: "Daily Visitor", quote: "The best mochi donuts in all of Florida.", tag: "Unique", avatarSrc: "http://img.b2bpic.net/free-photo/smiling-asian-girl-barista-giving-order-client-holding-takeaway-coffee-cup-wearing-apron-working_1258-199319.jpg", imageSrc: "http://img.b2bpic.net/free-photo/fried-chicken-meat-seafood-barbecue-with-vegetables-wooden-skewers-plate-sauce-top-view_1253-954.jpg?_wi=2", imageAlt: "happy customer dining cafe" }
       ]}
       title="Customer Reviews"
       description="Read what our amazing customers say about us."
